026adab60c94fa3aff95f01e52e6b98c785e9cb3
Matt Bellis <mbellis@siena.edu>
2020-07-25 04:12:12 -0400

    Update test_ROOT.py

--- test_ROOT.py
+++ test_ROOT.py
@@ -1,6 +1,7 @@
 import ROOT
 
-infilename = "root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root"
+#infilename = "root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root"
+infilename = "root://eospublic.cern.ch//eos/opendata/cms/derived-data/AOD2NanoAODOutreachTool/DYJetsToLL.root"
 
 f = ROOT.TFile.Open(infilename)
 
