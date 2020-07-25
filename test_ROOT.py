681233bc733f5f015ada0ac72485ebbf31f188f7
Matt Bellis <matthew.bellis@gmail.com>
2020-07-25 09:59:10 +0200

    Minor edits

--- test_ROOT.py
+++ test_ROOT.py
@@ -2,7 +2,7 @@ import ROOT
 
 infilename = "root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root"
 
-f = TFile.ROOT.Open(infilename)
+f = ROOT.TFile.Open(infilename)
 
 t = f.Get("Events")
 
@@ -12,7 +12,10 @@ print(nentries)
 
 for i in range(0,10):
 
-  t.GetEvent(i)
+  print(i)
+
+  #t.GetEntry(i)
+
 
 
 
