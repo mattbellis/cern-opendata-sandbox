f3cc0273772999ca494ec096dabd20a7f818584d
Matt Bellis <matthew.bellis@gmail.com>
2020-09-22 01:04:54 -0400

    Stuff

--- list_contents_of_directories.py
+++ list_contents_of_directories.py
@@ -4,29 +4,37 @@ import sys
 
 topdir = "root://eospublic.cern.ch//eos/opendata/cms/"
 
+print("To list the contents of any subdirectories of\n")
+print("{0}\n".format(topdir))
+print("just add them to the commandline options.\n")
+print("Example: ")
+print("python list_contents_of_directories.py")
+print("python list_contents_of_directories.py Run2011A")
+print("python list_contents_of_directories.py Run2011A/SingleMu")
+print("\n")
+
+
 if len(sys.argv)>1:
     topdir += sys.argv[1]
 
-#dire = ROOT.TSystemDirectory("root://eospublic.cern.ch//eos/opendata/cms/","root://eospublic.cern.ch//eos/opendata/cms/")
 dire = ROOT.TSystemDirectory(topdir,topdir)
 
 # TList object
 files = dire.GetListOfFiles()
 
 # For debugging
-files.Print()
+#files.Print()
 
 print("--------------------------------")
-print("Listing the contents of ")
-print()
+print("Listing the contents of\n")
 print(topdir)
-print("-------")
-print()
+print("-------\n")
+
+# For debugging
 #print(len(files))
 
 for f in files:
     print(f.GetName())
-    #print(f.IsDirectory())
 
 
 
